457cebc24bb2465583eb95bc6a1d08d3ab05b3f5
mitul1383 <mittulpattel@gmail.com>
2020-02-08 16:37:09 +0000

    homework

---
+++ src/printpractice/BioData.java
@@ -0,0 +1,13 @@
+package printpractice;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name = Mitul");
+        System.out.println("Address= Harrow");
+        System.out.println("Age36");
+        System.out.println("9,Duffeild Close");
+
+
+    }
+}
+
---
+++ src/printpractice/Star.java
@@ -0,0 +1,13 @@
+package printpractice;
+
+public class Star {
+    public static void main(String[] args){
+
+        System.out.println("----------");
+        System.out.println("  *  ");
+        System.out.println(" *   *");
+        System.out.println("* * * ");
+        System.out.println("----------");
+
+    }
+}
